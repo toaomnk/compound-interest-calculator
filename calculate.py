f7876dad88eeba5b8659a7e4e118b27a4504127c
toaomnk <46406634+toaomnk@users.noreply.github.com>
2019-02-02 04:25:53 -0600

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -3,7 +3,7 @@ from formulas import principal
 from formulas import rate
 from formulas import years
 
-print("\n[1] Accumulated Amount [2] Principal [3] Interest Rate [4] Years")
+print("[1] Accumulated Amount [2] Principal [3] Interest Rate [4] Years")
 while True:
 	try:
 		var = int(input("\nWhat are you solving for?: "))
