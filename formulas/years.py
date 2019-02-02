b47b06a2ce93cd1ade89ce0bde8f6efb80e7fc7d
toaomnk <46406634+toaomnk@users.noreply.github.com>
2019-02-02 14:54:04 -0600

    Add files via upload

--- formulas/years.py
+++ formulas/years.py
@@ -1,26 +1,30 @@
 import math
 
 def calculate():
-	a = float(input("\nEnter the Accumulated amount: $"))
-	p = float(input("\nEnter the Principal amount: $"))
-	r = float(input("\nEnter an Interest rate: %"))	
-	print("\n[1] Daily [2] Weekly [3] Monthly [4] Quarterly [5] Semi-Annually [6] Annually [7] Continuously")
-	c = int(input("\nEnter the rate of compounding: "))
-	if c == 1:
-		n = 365
-	elif c == 2:
-		n = 52
-	elif c == 3:
-		n = 12	
-	elif c == 4:
-		n = 4
-	elif c == 5:
-		n = 2
-	elif c == 6:
-		n = 1
-	elif c == 7:
-		n = 2.7182
-	answer = math.log10(a/p) / n * (math.log10(1 + (r/100)/n))
+	while True:
+			try:
+				a = float(input("\nEnter the Accumulated amount: $"))
+				break
+			except:
+				print("\nError: Improper value inputed.")
+	while True:
+		try:
+			p = float(input("\nEnter the Principal amount: $"))
+			if p == 0:
+				raise Exception("\nError: Cannot divide by zero.")
+			break
+		except:
+			print("\nError: Improper value inputed.")
+	while True:
+		try:
+			r = float(input("\nEnter an Interest rate: %"))
+			if r == 0:
+				raise Exception("\nError: Improper value inputed.")
+			break
+		except:
+			print("\nError: Improper value inputed.")
+
+	answer = math.log10(a/p) / (math.log10(1 + (r/100)))
 	print("\nThe number of years compounded is %s." % round(answer, 2))
 
 if __name__ == '__main__':
