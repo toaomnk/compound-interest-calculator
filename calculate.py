b5e97bd6081e1ae9a212fca82d1a956eaf4ef1c7
Johnathon Nguyen <46406634+toaomnk@users.noreply.github.com>
2019-12-12 22:33:13 -0600

    Update calculate.py
    Added comments

--- calculate.py
+++ calculate.py
@@ -3,25 +3,32 @@ from formulas import principal
 from formulas import rate
 from formulas import years
 
-print("[1] Accumulated Amount [2] Principal [3] Interest Rate [4] Years")
-while True:
+print("\n[1] Accumulated Amount [2] Principal [3] Interest Rate [4] Years") # Lists sub-calculators available.
+
+while True: # While loop allows program to continue running when exceptions occur.
 	try:
 		var = int(input("\nWhat are you solving for?: "))
-		if var > 4:
-			raise Exception("\nError: Improper value inputed.")
-		if var < 1:
-			raise Exception("\nError: Improper value inputed.")
+		if var > 4: # var cannot be out of range from 1-4.
+			raise Exception
+		elif var < 1:
+			raise Exception
 		break
 	except:
 		print("\nError: Improper value inputed.")
-		
-if var == 1:
-	accumulated_amount.calculate()
-elif var == 2:
-	principal.calculate()
-elif var == 3:
-	rate.calculate()
-elif var == 4:
-	years.calculate()
 
+if var == 1:
+	from Compound_Interest import accumulated_amount
+	accumulated_amount.calculate() # Initilization.
+
+elif var == 2:
+	from Compound_Interest import principal
+	principal.calculate() # Initilization.
+
+elif var == 3:
+	from Compound_Interest import rate
+	rate.calculate() # Initilization.
+	
+elif var == 4:
+	from Compound_Interest import years
+	years.calculate() # Initilization.
 input("\nPress ENTER to exit.")
