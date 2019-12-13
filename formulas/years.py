4d51f238d51ac13e306edb00409f36b323a830a9
Johnathon Nguyen <46406634+toaomnk@users.noreply.github.com>
2019-12-12 22:40:29 -0600

    Update years.py
    Added comments and more exceptions.

--- formulas/years.py
+++ formulas/years.py
@@ -1,31 +1,42 @@
 import math
 
 def calculate():
-	while True:
+
+	while True: # While loop allows program to continue running when exceptions occur.
 			try:
 				a = float(input("\nEnter the Accumulated amount: $"))
+				if a == 0:
+					raise Exception
+				elif a < 0:
+					raise Exception
 				break
 			except:
-				print("\nError: Improper value inputed.")
-	while True:
+				print("\nError: Improper value inputted.")
+
+	while True: # While loop allows program to continue running when exceptions occur.
 		try:
 			p = float(input("\nEnter the Principal amount: $"))
 			if p == 0:
-				raise Exception("\nError: Cannot divide by zero.")
+				raise Exception
+			elif p < 0:
+				raise Exception
 			break
 		except:
-			print("\nError: Improper value inputed.")
-	while True:
+			print("\nError: Improper value inputted.")
+
+	while True: # While loop allows program to continue running when exceptions occur.
 		try:
 			r = float(input("\nEnter an Interest rate: %"))
-			if r == 0:
-				raise Exception("\nError: Improper value inputed.")
+			if r < 0:
+				raise Exception
 			break
 		except:
-			print("\nError: Improper value inputed.")
+			print("\nError: Interest rate cannot be negative.")
+
 	answer = math.log10(a/p) / (math.log10(1 + (r/100)))
-	print("\nThe number of years compounded is %s." % round(answer, 2))
+	print("\nThe number of years compounded is roughly %s." % round(answer, 2))
+
 
 if __name__ == '__main__':
-	print("FileError: Unable to run directy, use calculator.py instead.\n")
+	print("FileError: Unable to run directy, use run.py instead.\n")
 	input("Press ENTER to exit.")
